43afaeb0147aeaa5f4bb41850125786438638c61
sansSpoon <sans.spoon@gmail.com>
2018-05-21 09:42:29 +1000

    add base componemts

---
+++ src/components/planet/planet.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Planet extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		};
+	}
+
+	render() {
+		return (
+			
+		);
+	}
+
+}
