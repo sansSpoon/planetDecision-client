d0289c59bea3e12efe3dc8e19996ac891f540fe9
sansSpoon <sans.spoon@gmail.com>
2018-05-21 15:38:24 +1000

    add, add Hierarchy

--- src/components/system/system.js
+++ src/components/system/system.js
@@ -5,8 +5,32 @@ export default class System extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			
+			hierarchyName: '',
+			hierarchy: [],
 		};
+		
+		this.handleAddHierarchy = this.handleAddHierarchy.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleChange(event) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+		this.setState({[name]: value});
+	}
+	
+	handleAddHierarchy(event) {
+		console.log(event);
+		const newHierarchy = {
+			name: this.state.hierarchyName,
+			star: '',
+			planets: [],
+		}
+		console.log(newHierarchy);
+		this.setState((prevState) => ({
+			hierarchy: [...prevState.hierarchy, newHierarchy]
+		}));
 	}
 
 	render() {
@@ -18,10 +42,13 @@ export default class System extends Component {
 				</div>
 				<div>
 					<label htmlFor="hierarchy">Hierarchy</label>
-					<input id="hierarchy" name="hierarchy" type="text" value={this.state.hierarchy} onChange={this.handleChange} />
+					<input id="hierarchy" name="hierarchyName" type="text" value={this.state.hierarchyName} onChange={this.handleChange} />
 				</div>
+				<input name="addHierarchy" value="Add Hierarchy" type="button" onClick={this.handleAddHierarchy} />
 			</form>
 		);
+		
+		
 	}
 
 }
