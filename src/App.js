e6e8a163913520d3d06b7341250fd5d0b533b5bd
sansSpoon <sans.spoon@gmail.com>
2018-05-21 13:11:15 +1000

    add basic navigation to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import System from './components/system/system';
+import Hierarchy from './components/hierarchy/hierarchy';
+import Star from './components/star/star';
+import Planet from './components/planet/planet';
+import Satellite from './components/satellite/satellite';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			nav: 'system',
+		};
+		
+		this.handleNav = this.handleNav.bind(this);
+	}
+	
+	handleNav(event) {
+		const target = event.target;
+		const name = target.name;
+		this.setState({'nav': name});
+	}
+	
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Planet Decision</h1>
+				</header>
+				<nav>
+					<button name="system" onClick={this.handleNav}>System</button>
+					<button name="hierarchy" onClick={this.handleNav}>Hierarchy</button>
+					<button name="star" onClick={this.handleNav}>Star</button>
+					<button name="planet" onClick={this.handleNav}>Planet</button>
+					<button name="satellite" onClick={this.handleNav}>Satellite</button>
+				</nav>
+				<section>
+					{{
+						system: <System />,
+						hierarchy: <Hierarchy />,
+						star: <Star />,
+						planet: <Planet />,
+						satellite: <Satellite />,
+					}[this.state.nav]}
+				</section>
+			</div>
+		);
+	}
 }
 
 export default App;
