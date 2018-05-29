ea1660c5da541ca3f0d3493ae3407a0a0712d59d
sansSpoon <sans.spoon@gmail.com>
2018-05-29 14:13:31 +1000

    finish star CRUD

--- src/components/star/star.js
+++ src/components/star/star.js
@@ -16,6 +16,7 @@ export default class Star extends Component {
 				message: '',
 			},
 			ui: {
+				currentStar: '',
 				stars: [],
 			},
 		};
@@ -31,6 +32,36 @@ export default class Star extends Component {
 		this.setState({data: {...this.state.data, [name]: value }});
 	}
 	
+	handleActive(id) {
+		if(id) {
+			this.setState(
+				{ data: { ...this.state.data,
+						name: id.name,
+						radiusKM: id.radiusKM,
+						rotationVelocityKMH: id.rotationVelocityKMH,
+						_id: id._id,
+					},
+					ui: { ...this.state.ui,
+						currentStar: id,
+					},
+				},
+			);
+		} else {
+			this.setState(
+				{ data: { ...this.state.data,
+						name: '',
+						radiusKM: '',
+						rotationVelocityKMH: '',
+						_id: '',
+					},
+					ui: { ...this.state.ui,
+						currentStar: '',
+					},
+				},
+			);
+		}
+	}
+	
 	handleSave(event) {
 		event.preventDefault();
 		
@@ -38,7 +69,7 @@ export default class Star extends Component {
 			payload = this.state.data,
 			init = {
 				body: JSON.stringify(payload),
-				method: 'POST',
+				method: this.state.ui.currentStar ? 'PUT' : 'POST',
 				mode: 'cors',
 				cache: 'default',
 				headers: {
@@ -47,11 +78,12 @@ export default class Star extends Component {
 				}
 			};
 		
-		fetch(apiBaseUri, init)
+		fetch(`${apiBaseUri}${this.state.ui.currentStar ? this.state.ui.currentStar._id : ''}`, init)
 			.then(inspectResponse)
 			.then(({status, data}) => {
 				if (status >= 200 && status <= 299) {
-					console.log(data, status);
+					this.handleActive();
+					this.handleGetStars();
 				} else {
 					this.setState({
 						messages: { ...this.state.messages, status: status, message: data.message }
@@ -96,6 +128,33 @@ export default class Star extends Component {
 	handleAddStar(id) {
 		this.props.addStar(id);
 	}
+	
+	handleDeleteStar(id) {
+	
+		const apiBaseUri = "http://localhost:3001/stars/",
+			init = {
+				method: 'DELETE',
+				mode: 'cors',
+				cache: 'default',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+				}
+			};
+		
+		fetch(`${apiBaseUri}${id}`, init)
+			.then((response) => {
+				if(!response.ok) {
+					this.setState(
+						{	messages: { ...this.state.messages, status: response.status, message: "Delete failed." } }
+					);
+				}
+				this.handleGetStars();
+			})
+			.catch(error => {
+				console.log('There has been a problem with the fetch operation: ', error.message);
+			});
+	}
 
 	componentDidMount() {
 		this.handleGetStars();
@@ -106,8 +165,8 @@ export default class Star extends Component {
 		const stars = this.state.ui.stars.map((item) => {
 			return (
 				<li key={item._id}>{item.name}
-					<input name="edit" value="Edit" type="button" onClick={() => this.handleActivePlanet(item)} />
-					<input name="delete" value="Delete" type="button" onClick={() => this.handleDeletePlanet(item._id)} />
+					<input name="edit" value="Edit" type="button" onClick={() => this.handleActive(item)} />
+					<input name="delete" value="Delete" type="button" onClick={() => this.handleDeleteStar(item._id)} />
 					<input name="add" value="Add to Hierarchy" type="button" onClick={() => this.handleAddStar(item._id)} />
 				</li>
 			);
@@ -129,7 +188,7 @@ export default class Star extends Component {
 						<label htmlFor="rotation">Rotation (km/h)</label>
 						<input id="rotation" name="rotationVelocityKMH" type="text" value={this.state.data.rotationVelocityKMH} onChange={this.handleChange} />
 					</div>
-					<input name="save" value="save" type="submit" onClick={this.handleSave} />
+					<input name="save" value={(this.state.ui.currentStar)?"Update":"Save"} type="submit" onClick={this.handleSave} />
 				</form>
 				<div>
 					<ul>{ stars }</ul>
